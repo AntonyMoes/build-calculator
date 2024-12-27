418a3ad25e4a3ee20c242a176dfe01541d94572e
oncefirefly <oncefirefly@gmail.com>
2024-12-27 21:05:36 +0300

    fix: Popup template

---
+++ src/components/common/Popup.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+// inside parent component
+// const isOpen = ref(false);
+
+defineProps<{
+  isOpen: boolean;
+}>();
+
+defineEmits<{
+  (e: "close");
+}>();
+</script>
+
+<template lang="">
+  <div v-if="isOpen">
+    <!-- POPUP HUETA -->
+     <slot></slot>
+    <MyButton onClick="$emit('close')" />
+  </div>
+</template>
+
+<style lang="">
+
+</style>
